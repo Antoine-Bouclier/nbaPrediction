eb2e46d2cb1f9ce87a504ad1eb8bfa1d4e7659da
Antoine-Bouclier <antoinebouclier1@gmail.com>
2024-03-06 11:45:52 +0100

    Adding the entity WestFranchise

---
+++ src/Entity/WestFranchise.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\WestFranchiseRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: WestFranchiseRepository::class)]
+class WestFranchise
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $town = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::BLOB)]
+    private $logo = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTown(): ?string
+    {
+        return $this->town;
+    }
+
+    public function setTown(string $town): static
+    {
+        $this->town = $town;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getLogo()
+    {
+        return $this->logo;
+    }
+
+    public function setLogo($logo): static
+    {
+        $this->logo = $logo;
+
+        return $this;
+    }
+}
